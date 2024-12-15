8e2b7059e97d6ccfa8c425a247399cf78062e4b0
Riel <123573741+kitaroriel@users.noreply.github.com>
2024-12-15 20:28:46 +0700

    Add Providers

---
+++ app/providers.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import { useEffect, useState } from "react"
+
+export function Providers({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return <>{children}</>
+  }
+
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem {...props}>
+      {children}
+    </NextThemesProvider>
+  )
+}
+
